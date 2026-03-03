51ce7758f0bfd195b956460638b3b71f50bd360b
1302323-blip <1302323@spsd.sk.ca>
2026-03-03 15:25:01 -0600

    Update sketch.js

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -8,6 +8,7 @@ function setup() {
 
 function draw() {
   background(220);
+  noStroke;
 
   for (let ball of ballArray){
     // move
@@ -15,7 +16,16 @@ function draw() {
     ball.y += ball.dy;
 
     //display
+    noStroke();
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius * 2);
+
+    if (ball.x < ball.radius || ball.x > width - ball.radius){
+      ball.dx *= -1;
+    }
+    if (ball.y < ball.radius || ball.y > height - ball.radius){
+      ball.dy *= -1;
+    }
   }
 }
 
@@ -30,6 +40,10 @@ function spawnBall(){
     dx: random(-5, 5),
     dy: random (-5, 5),
     radius: random(10, 40),
+    
+    r: random(255),
+    g: random(255),
+    b: random(255),
   };
   ballArray.push(theBall);
 }
