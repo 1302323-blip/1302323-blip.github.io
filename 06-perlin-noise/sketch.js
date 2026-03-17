8bb1a8e434287c01650736c393d00150448e60a3
1302323-blip <1302323@spsd.sk.ca>
2026-03-17 14:36:46 -0600

    Perlin Noise Demo

---
+++ 06-perlin-noise/sketch.js
@@ -0,0 +1,22 @@
+// Perlin Noise Demo
+
+let time = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  ball();
+}
+
+function ball(){
+  fill("black");
+  let x = noise(time) * width;
+  let y = noise(time + 1000) * height;
+  circle(x, y, 75);
+
+  time += 0.01;
+}
