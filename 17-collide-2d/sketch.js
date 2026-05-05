137d68cf904becaedbe2938b654cbc7e8d95d74b
1302323-blip <1302323@spsd.sk.ca>
2026-05-05 14:34:15 -0600

    collide 2d demo; libraries

---
+++ 17-collide-2d/sketch.js
@@ -0,0 +1,24 @@
+// Collide 2D Demo
+
+let hit;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  hit = collideRectCircle(200, 200, 100, 150, mouseX, mouseY, 100);
+  console.log(hit);
+
+  if (hit){
+    fill("blue");
+  }
+  else {
+    fill("white");
+  }
+
+  rect(200, 200, 100, 150);
+  circle(mouseX, mouseY, 100);
+}
